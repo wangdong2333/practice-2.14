8a281e255c5bad382ac720c23f087359fee08ea9
wangdong222 <1273179707@qq.com>
2019-03-27 09:19:37 +0800

    移动端demo

--- 移动端/demo例子/index.js
+++ 移动端/demo例子/index.js
@@ -1,25 +1,36 @@
 var iNow = 1;
-$(document).on('swipeUp',function(){
-    // console.log(1111111);
-    $('.page-'+iNow).attr('class','page-'+iNow+' moveToTop');
 
-    $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom"  );
- 
-    setTimeout(function(){
-        $('.page-'+(iNow+1)).children().removeClass('hide');
-        // 滑出的中的小图片 隐藏
-        $('.page-'+iNow).children().addClass('hide');
-        iNow++;
-    },600)
-})
+    $(document).on('swipeUp',function(){
+        if(iNow !=5){
+            // console.log(1111111);
+        $('.page-'+iNow).attr('class','page-'+iNow+' moveToTop');
+    
+        $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom"  );
+     
+        setTimeout(function(){
+            $('.page-'+(iNow+1)).children().removeClass('hide');
+            // 滑出的中的小图片 隐藏
+            $('.page-'+iNow).children().addClass('hide');
+            iNow++;
+        },600)
+        }
+        
+    }) 
 
-$(document).on('swipeDown',function(){
-    $('.page-'+iNow).attr('class','page-'+iNow+" moveToBottom");
-    $('.page-'+(iNow-1)).attr('class','page-'+(iNow-1)+" moveFromTop");
-    setTimeout(function(){
-        $('.page-'+(iNow-1)).children().removeClass('hide');
-        // 下滑的时候 让出去的小图都隐藏
-        $('.page-'+iNow).children().addClass('hide');
-        iNow--;
-    }, 600);
-})
+
+    $(document).on('swipeDown',function(){
+        if(iNow !=1){
+            $('.page-'+iNow).attr('class','page-'+iNow+" moveToBottom");
+            $('.page-'+(iNow-1)).attr('class','page-'+(iNow-1)+" moveFromTop");
+            setTimeout(function(){
+                $('.page-'+(iNow-1)).children().removeClass('hide');
+                // 下滑的时候 让出去的小图都隐藏
+                $('.page-'+iNow).children().addClass('hide');
+                iNow--;
+            }, 600);
+        }
+       
+    })
+
+    
+
