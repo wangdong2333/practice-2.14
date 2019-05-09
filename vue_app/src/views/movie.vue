c5811313e2691314c78650883a56b02593f988e4
wangdong222 <1273179707@qq.com>
2019-05-09 16:08:36 +0800

    movie   内容  5-9

--- vue_app/src/views/movie.vue
+++ vue_app/src/views/movie.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-        电影
-    </div>
+  <div>
+    <ul >
+      <li class="movie-list">
+        <img class="movie-img" src="xx.jpg" alt>
+
+        <div class="movie-text">
+          <h4>复仇者联盟4：终极之战</h4>
+          <p>小罗伯特.唐尼/克里斯.埃文斯</p>
+          <p>590630人已观看</p>
+          <p>年份：2019</p>
+          <span>动作/科幻/奇幻/</span>
+        </div>
+      </li>
+      <li class="movie-list">
+        <img class="movie-img" src="xx.jpg" alt>
+
+        <div class="movie-text">
+          <h4>复仇者联盟4：终极之战</h4>
+          <p>小罗伯特.唐尼/克里斯.埃文斯</p>
+          <p>590630人已观看</p>
+          <p>年份：2019</p>
+          <span>动作/科幻/奇幻/</span>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style  scoped>
+    .movie-list{
+        display: flex;
+        border-bottom: 1px solid #000;
+        padding:0.2rem 0;
+    }
+    .movie-img{
+        width: 100px;
+        height: 150px;
+        margin-right: 0.2rem;
+    }
+    .movie-text{
+        flex:1;
 
+    }
 </style>
