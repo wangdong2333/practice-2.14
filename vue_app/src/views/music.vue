bd487aa9e57255dba7aac0927356107b28448aee
wangdong222 <1273179707@qq.com>
2019-05-10 19:09:28 +0800

    music  图片 5-10

--- vue_app/src/views/music.vue
+++ vue_app/src/views/music.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        音乐
+        <img v-for="(obj,index) in musicList" :key="index" :src="obj.bg" alt="">
+        <img :src="obj.bg" v-for="(obj,index) in musicList" :key="index" @click="goList()"/>
+
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        
+        data() {
+            return {        
+                musicList:[],
+
+            }
+        },
+         created() {
+            axios.get('/data/musiclist.json')
+            .then((result)=>{
+                this.musicList = result.data.albums;
+            })
+        },
     }
 </script>
 
