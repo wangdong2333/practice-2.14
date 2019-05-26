c7dc6680fe89165db14022b02c75bc61efac0ab8
wangdong222 <1273179707@qq.com>
2019-05-26 15:53:41 +0800

    Easy Mock

--- antd-demo/src/page/Student/index.js
+++ antd-demo/src/page/Student/index.js
@@ -1,11 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Table } from 'antd';
+import axios from 'axios';
 
-export default class Student extends Component {
+class Student extends Component {
+    constructor(params) {
+        super(params);
+        this.state = {
+            dataSource:[]
+        }
+    }
+    componentWillMount() {
+        axios.get(' https://www.easy-mock.com/mock/5cea404232f9f3098f832dcb/demo/student')
+        .then((res)=>{
+            this.setState({
+                dataSource:res.data.data.projects
+            })
+        })
+    }
+    
     render() {
+        const columns = [
+            {
+                title: '姓名',
+                dataIndex: 'name',
+                key: 'name',
+              },
+              {
+                title: '邮箱',
+                dataIndex: 'email',
+                key: 'email',
+              },
+              {
+                title: '住址',
+                dataIndex: 'address',
+                key: 'address',
+              },{
+                title: '学号',
+                dataIndex: 'id',
+                key: 'id',
+              },
+              ,{
+                title: '学校',
+                dataIndex: 'school',
+                key: 'school',
+              }
+        ]
+       
         return (
-            <div>
-                学生列表
+            <div style={{background:"#fff"}}>
+                <Table columns={columns}  dataSource={this.state.dataSource}></Table>
             </div>
-        )
+        );
     }
 }
+
+export default Student;
