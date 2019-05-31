9a95765ce28b82bcd683039d11f775c3a28425c1
wangdong222 <1273179707@qq.com>
2019-05-31 17:58:19 +0800

    node 1-test

--- Node/1-test.js
+++ Node/1-test.js
@@ -1,3 +1,4 @@
+// console.log("123")
 //console.log("xx");
 console.log("start");
 for(var i=0;i<5;i++){
@@ -9,9 +10,9 @@ console.log("end");
 
 
 /*
-start-->end-->  0 1 2 3 4 
-                4 4 4 4 4
-                5 5 5 5 5 */
+start-->end-->  0 1 2 3 4 (x)
+                4 4 4 4 4 (X)
+                5 5 5 5 5 (√)*/
 异步代码---event loop
 1、当前线程空闲
 2、满足触发条件
