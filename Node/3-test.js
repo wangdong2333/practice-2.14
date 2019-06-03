857d42eac637b2a671ffc36e86faae51e31b5576
wangdong222 <1273179707@qq.com>
2019-06-03 17:25:36 +0800

    继承

--- Node/3-test.js
+++ Node/3-test.js
@@ -4,8 +4,8 @@ http.createServer(function(req,res){
 	res.writeHead(200,{"Content-type":"text/html"});
 	//res.write("<h1>hello world</h1>");
 	//res.end();
-	res.end("<h1>hello world</h1>");
+	res.end("<h2>hello world</h2>");
 }).listen(3000);
 
 
-console.log("server start port 3000");
+console.log("server start port 3000"); 
