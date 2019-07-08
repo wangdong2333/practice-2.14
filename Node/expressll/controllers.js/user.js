f846e5352ada254222ddbdf0a9a0dc32e4f88b98
wangdong222 <1273179707@qq.com>
2019-07-08 10:16:44 +0800

    连通数据库

--- Node/expressll/controllers.js/user.js
+++ Node/expressll/controllers.js/user.js
@@ -1,4 +1,14 @@
 // var User_model=require("../models/user_model.js");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'newblog'
+  });
+   
+ 
+
 exports.reg=function(req,res,next){
 	res.render("reg.ejs");
 }
@@ -6,8 +16,18 @@ exports.reg=function(req,res,next){
 exports.do_reg=function(req,res,next){
 	var name = req.body.uname;
 	var pass = req.body.pass;
-	console.log(name);
-	console.log(pass);
+	// console.log(name);
+	// console.log(pass);
+	connection.connect();
+   
+	connection.query('SELECT * from user', function (error, results, fields) {
+	  if (error) throw error;
+	  //console.log('The solution is: ', results[0].solution);
+	  console.log(results);
+	  connection.end();
+	});
+	 
+	
 }
 
 
