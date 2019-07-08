afbefb66f75f0d4c3f0ba76798c7247ba33a4961
wangdong222 <1273179707@qq.com>
2019-07-08 10:03:42 +0800

    登录页面

--- Node/expressll/controllers.js/user.js
+++ Node/expressll/controllers.js/user.js
@@ -1,31 +1,14 @@
-var User_model=require("../models/user_model.js");
+// var User_model=require("../models/user_model.js");
 exports.reg=function(req,res,next){
 	res.render("reg.ejs");
 }
 
 exports.do_reg=function(req,res,next){
-	var name=req.body.uname;
-	var pass=req.body.pass;
-	//console.log(name);
-	//console.log(pass);
-	User_model.insert_data(name,pass,function(err,data){
-		//console.log(data);
-		if(data.affectedRows>0){
-			res.redirect('/login');
-		}
-	})	
+	var name = req.body.uname;
+	var pass = req.body.pass;
+	console.log(name);
+	console.log(pass);
 }
 
-exports.login=function(req,res,next){
-	res.render("login.ejs");
-}
 
-exports.do_login=function(req,res,next){
-	var name=req.body.uname;
-	var pass=req.body.pass;
 
-	User_model.sel_name_by_pass(name,pass,function(err,data){
-		console.log(data);
-	})
-
-}
--- Node/expressll/routes/index.js
+++ Node/expressll/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../controllers.js/user.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/reg', function(req, res, next) {
-  res.render("reg.ejs");
-})
+router.get('/reg',User.reg );
+router.post('/reg',User.do_reg);
 
 module.exports = router;
