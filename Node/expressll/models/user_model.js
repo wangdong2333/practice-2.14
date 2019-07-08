de978b4e7e4460c44678deb1cc95838f03511009
wangdong222 <1273179707@qq.com>
2019-07-08 13:32:58 +0800

    封装数据库代码

--- Node/expressll/models/user_model.js
+++ Node/expressll/models/user_model.js
@@ -1,4 +1,14 @@
 //存放数据库代码
+
+
+var db=require("./db.js");
+
+exports.insert_data=function(name,pass,callback){
+	var sql="insert into user(uname,pass) values(?,?)";
+	db.query(sql,[name,pass],callback);
+}
+
+/*
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
 	host     : 'localhost',
@@ -20,3 +30,28 @@ exports.insert_data=function(name,pass,callback){
         connection.end();
     });  
 }
+*/
+
+/*
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '',
+  database        : 'newblog'
+});
+
+exports.insert_data=function(name,pass,callback){
+	pool.getConnection(function(err, connection) {
+	  if (err) throw err; 
+	  var sql="insert into user(uname,pass) values(?,?)";
+	  connection.query(sql,[name,pass],function (error, results, fields) {
+	    connection.release();
+	    if (error) throw error;
+	    callback(error,results);
+	  });
+	});
+}
+*/
+
