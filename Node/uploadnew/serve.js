570c63340b61fef416f232627c130c51050ad414
wangdong222 <1273179707@qq.com>
2019-07-13 11:26:45 +0800

    上传文件成功

--- Node/uploadnew/serve.js
+++ Node/uploadnew/serve.js
@@ -1,6 +1,7 @@
 var http=require('http');
 var url=require('url');
 var fs = require('fs');
+var util = require('util');
 var formidable=require('formidable');
 
 http.createServer(function(req,res){
@@ -22,10 +23,31 @@ console.log("serve start port 3000");
 
 function goIndex(res){
     //读取路径
-    var pathname=__dirname+"/static/"+url.parse("index.html").parse;
+    var pathname=__dirname+"/static/"+url.parse("index.html").pathname;
     //读取文件（把文件加载到内存当中）
     var realf=fs.readFileSync(pathname,'utf-8');
     //把文件打包成数据回传
-    res.writeHead(200,{"Content-type":"text/plain"});
+    res.writeHead(200,{"Content-type":"text/html"});
     res.end(realf);
+}
+function  goUpload(res,req){
+    var form = new formidable.IncomingForm();
+    form.uploadDir="./upload";
+
+    form.parse(req, function(err, fields, files) {
+        var oldurl=files.sfile.path;
+        var newurl=__dirname+"/upload/"+files.sfile.name;
+        console.log(files.sfile.path);
+        console.log(files.sfile.name);
+        fs.rename(oldurl,newurl,function(err){
+            if(err) throw err;
+            res.writeHead(200,{"Content-type":"text/plain"});
+            res.end("上传文件成功");
+        })
+        /*
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));*/
+    });
+    return;
 }
