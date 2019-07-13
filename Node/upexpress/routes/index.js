2edbf8264c1d3f2680a384b45c9ddf52f36ac10d
wangdong222 <1273179707@qq.com>
2019-07-13 11:42:28 +0800

    下载  formidable

--- Node/upexpress/routes/index.js
+++ Node/upexpress/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var util = require('util');
+var formidable = require('formidable');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/parse',function(res,req,next){
+  var form = new formidable.IncomingForm();
+ 
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+    return;
+});
 module.exports = router;
