6f70a7e9dcf497fc74edd5b74149a9f3e5ba2254
wangdong222 <1273179707@qq.com>
2019-11-04 22:26:16 +0800

    setTimeout

--- 总复习/13_任务队列.js
+++ 总复习/13_任务队列.js
@@ -1,38 +1,38 @@
-//  console.log('1');
+ console.log('1');
  setTimeout(function() {
-    // console.log('2');
+    console.log('2');
     process.nextTick(function() {
         console.log('3');
     })
     new Promise(function(resolve) {
-        // console.log('4');
+        console.log('4');
         resolve();
     }).then(function() {
         console.log('5')
     })
+},1000)
+process.nextTick(function() {
+    console.log('6');
 })
-// process.nextTick(function() {
-//     console.log('6');
-// })
-// new Promise(function(resolve) {
-//     console.log('7');
-//     resolve();
-// }).then(function() {
-//     console.log('8')
-// })
+new Promise(function(resolve) {
+    console.log('7');
+    resolve();
+}).then(function() {
+    console.log('8')
+})
 
 setTimeout(function() {
-    // console.log('9');
+    console.log('9');
     process.nextTick(function() {
         console.log('10');
     })
     new Promise(function(resolve) {
-        // console.log('11');
+        console.log('11');
         resolve();
     }).then(function() {
         console.log('12')
     })
-})
+},1000)
 
 //1 7 6 8 2 4 3 5 9 11 10 12
 //https://www.cnblogs.com/oicb/p/10615592.html
