936c0897d64563326794c74fedffe946e06960b7
wangdong222 <1273179707@qq.com>
2019-12-06 20:08:50 +0800

    复习

--- 总复习/13_任务队列.js
+++ 总复习/13_任务队列.js
@@ -1,16 +1,19 @@
  console.log('1');
  setTimeout(function() {
     console.log('2');
-    process.nextTick(function() {
-        console.log('3');
-    })
+    // process.nextTick(function() {
+    //     console.log('3');
+    // })
     new Promise(function(resolve) {
         console.log('4');
         resolve();
     }).then(function() {
         console.log('5')
+    }).then(() =>{
+        console.log(3);
     })
 },1000)
+
 process.nextTick(function() {
     console.log('6');
 })
@@ -23,14 +26,16 @@ new Promise(function(resolve) {
 
 setTimeout(function() {
     console.log('9');
-    process.nextTick(function() {
-        console.log('10');
-    })
+    // process.nextTick(function() {
+    //     console.log('10');
+    // })
     new Promise(function(resolve) {
         console.log('11');
         resolve();
     }).then(function() {
         console.log('12')
+    }).then(() =>{
+        console.log('10')
     })
 },1000)
 
